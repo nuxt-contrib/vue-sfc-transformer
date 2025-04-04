89a523ec5c1fa781baeab8767bd9f7a761e87127
Daniel Roe <daniel@roe.dev>
2025-04-04 12:02:13 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -20,7 +20,43 @@ pnpm install vue-sfc-transformer
 ```
 
 ```js
-import {} from 'vue-sfc-transformer'
+import { parse as parseDOM } from '@vue/compiler-dom'
+import { parse as parseSFC } from '@vue/compiler-sfc'
+import { transform } from 'esbuild'
+
+import { preTranspileScriptSetup, transpileVueTemplate } from 'vue-sfc-transformer'
+
+const src = `
+<template>
+  <div v-if="test as any" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  test?: string
+}>()
+</script>
+`
+
+const transpiledTemplate = await transpileVueTemplate(
+  src,
+  parseDOM(src, { parseMode: 'base' }),
+  0,
+  async (code) => {
+    const res = await transform(code, { loader: 'ts', target: 'esnext' })
+    return res.code
+  },
+)
+
+console.log(transpiledTemplate)
+
+const sfc = parseSFC(transpiledTemplate, {
+  filename: 'test.vue',
+  ignoreEmpty: true,
+})
+
+const { content: scriptBlockContents } = await preTranspileScriptSetup(sfc.descriptor, 'test.vue')
+console.log(scriptBlockContents)
 ```
 
 ## 💻 Development
--- playground/index.js
+++ playground/index.js
@@ -1,7 +1,37 @@
-import assert from 'node:assert'
-import * as pkg from 'vue-sfc-transformer'
+import { parse as parseDOM } from '@vue/compiler-dom'
+import { parse as parseSFC } from '@vue/compiler-sfc'
+import { transform } from 'esbuild'
 
-// eslint-disable-next-line no-console
-console.log(pkg.welcome())
+import { preTranspileScriptSetup, transpileVueTemplate } from 'vue-sfc-transformer'
 
-assert.strictEqual(pkg.welcome(), 'hello world')
+const src = `
+<template>
+  <div v-if="test as any" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  test?: string
+}>()
+</script>
+`
+
+const transpiledTemplate = await transpileVueTemplate(
+  src,
+  parseDOM(src, { parseMode: 'base' }),
+  0,
+  async (code) => {
+    const res = await transform(code, { loader: 'ts', target: 'esnext' })
+    return res.code
+  },
+)
+
+console.log(transpiledTemplate)
+
+const sfc = parseSFC(transpiledTemplate, {
+  filename: 'test.vue',
+  ignoreEmpty: true,
+})
+
+const { content: scriptBlockContents } = await preTranspileScriptSetup(sfc.descriptor, 'test.vue')
+console.log(scriptBlockContents)
