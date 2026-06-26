4a70fb4705439c6d4477cb7fb3d6223ee7781ba4
Alexander Lichter <github@lichter.io>
2026-06-26 08:45:44 +0200

    docs: note ignored directive modifiers

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -115,6 +115,7 @@ function handleNode(
             ]
           : [node.exp],
         // node.arg,
+        // ..node.modifiers, #273
       ].filter(item => !!item)
       for (const child of nodes) {
         search(child)
